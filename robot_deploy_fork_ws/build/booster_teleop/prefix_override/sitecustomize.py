312889cbe0147ebcb3404a0ba1e4d5946f5e8b2f
Pathfinder <sweetiestarseeker@gmail.com>
2026-03-21 20:25:05 +0300

    impl new launch file booster_teleop launch

--- robot_deploy_fork_ws/build/booster_teleop/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/pathfinder/Projects/Starkit/booster_teleop/robot_deploy_fork_ws/install/booster_teleop'
